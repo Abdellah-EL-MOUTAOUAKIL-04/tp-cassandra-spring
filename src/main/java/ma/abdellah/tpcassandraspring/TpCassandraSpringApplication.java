8c3a7f268ed9e3b50b8035b4922838f854c46676
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-10 14:53:22 +0000

    init

---
+++ src/main/java/ma/abdellah/tpcassandraspring/TpCassandraSpringApplication.java
@@ -0,0 +1,13 @@
+package ma.abdellah.tpcassandraspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpCassandraSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpCassandraSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/abdellah/tpcassandraspring/TpCassandraSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.abdellah.tpcassandraspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpCassandraSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
